b36fbf3d802651adb0508d5e326c9479c9c9646b
lee jae gyu <ljk950327@gmail.com>
2017-08-23 10:59:16 +0900

    P1110 P4344

--- acm-icpc/boj/src/P1546.java
+++ acm-icpc/boj/src/P1546.java
@@ -24,5 +24,26 @@ public class P1546 {
 		System.out.printf("%.2f", avr/ar.get(ar.size()-1)*100);
 		
 	}
+//	public class Main {
+//	    static int MAX_N = 1001;
+//	    static int N;
+//	    static double MAX;
+//	    static int SUM;
+//	    static int[] SCORE = new int[MAX_N];
+//	    
+//	    public static void main(String[] args) throws Exception{
+//	        //System.setIn(new FileInputStream("input.txt"));
+//	        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//	        N = Integer.parseInt(in.readLine());
+//	        
+//	        StringTokenizer st = new StringTokenizer(in.readLine());
+//	        for(int idx = 0; idx < N; idx++) {
+//	            SCORE[idx] = Integer.parseInt(st.nextToken());
+//	            SUM += SCORE[idx];
+//	            MAX = Math.max(MAX, SCORE[idx]);
+//	        }
+//	        System.out.printf("%.2f\n", (SUM * 100 / MAX / N));
+//	    }
+//	}
 
 }
